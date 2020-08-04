937f79434c44c2eddf8d24b0a01801e6e6ba8304
jPee2k <kipishept@gmail.com>
2020-08-04 12:48:49 +0300

    lesson6

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
     // Эти методы дают возможность работать
     // с сущностью через методы классов:
     // $post = Post::find(1);
--- app/Tag.php
+++ app/Tag.php
@@ -3,8 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Tag extends Model
 {
-    //
+    use Sluggable;
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Post::class,    // модель связи
+            'post_tags',    // таблица связи
+            'tag_id',       // id этой модели
+            'post_id'       // id связываемой модели
+        );
+    }
 }
